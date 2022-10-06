ae6beb2a59faf6a433fc32f161d66dac6c08e3c4
sebas <sebas@172.16.218.64>
2022-10-06 12:49:55 -0500

    +Asignatura

---
+++ src/main/java/Unidad01_1/Auto.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Unidad01_1;
+
+/**
+ *
+ * @author sebas
+ */
+public class Auto {
+    String marca;
+    int año;
+    String placa;
+}
