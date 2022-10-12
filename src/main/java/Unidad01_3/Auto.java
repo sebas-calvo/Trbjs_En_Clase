02d2acaaba8d8fa6ce875e636c5423be6554649f
sebas <sebas@172.16.218.64>
2022-10-12 12:31:53 -0500

    comandos marcas

---
+++ src/main/java/Unidad01_3/Auto.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Unidad01_3;
+
+/**
+ *
+ * @author sebas
+ */
+public class Auto {
+
+    String marca;
+    String placa;
+    String nombrePropietario;
+
+    public String mostrarInfo() {
+        var retorno = " El auto tiene placa: "+this.placa+" su marca es "
+                +this.marca+" y su propietario es: "+this.nombrePropietario;
+        
+        return retorno;
+    }
+        
+    public String getProvincia() {
+        var retorno = "";
+        return retorno;
+       
+    } 
+    public String getPaisMarca() {
+        var retorno = "";
+        if (this.marca.equals("Chevrolet")) 
+            retorno = "EE.UU";
+        else 
+        if (this.marca.equals("Toyota")) 
+                retorno = "Japón";
+            else 
+            if (this.marca.equals("Jetour"))
+                retorno="China";
+                else
+                if (this.marca.equals("Mercedez Benz"))
+                retorno="Alemania";
+
+        else
+        retorno = "No definido";
+            
+        
+    return retorno;
+    }
+        
+    
+public int getNumeroContinente() {
+        var retorno=0;
+        
+        return retorno;
+        
+        
+    }
+
+}
