1b1b0ba0588ecaa40ea3311793eb9b4e67855301
sebas <sebas@172.16.218.64>
2022-10-12 12:42:55 -0500

    metodos marcas+pagani

---
+++ src/main/java/Unidad01_3/Principal.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Unidad01_3;
+
+/**
+ *
+ * @author sebas
+ */
+public class Principal {
+    public static void main(String [] args){
+        
+        var auto1 = new Auto();
+        auto1.placa="ABF8592";
+        auto1.marca="Chevrolet";
+        auto1.nombrePropietario="Juana Moreno";
+
+        System.out.println(auto1.mostrarInfo()+" y el pais de origen es "+auto1.getPaisMarca());
+    
+    
+        var auto2 = new Auto();
+        auto2.placa="GKA2131";
+        auto2.marca="Toyota";
+        auto2.nombrePropietario="Juanito";
+
+        System.out.println(auto2.mostrarInfo()+" y el pais de origen es "+auto2.getPaisMarca());
+
+
+        var auto3 = new Auto();
+        auto3.placa="UBQ898";
+        auto3.marca="Mercedez Benz";
+        auto3.nombrePropietario="Martin Eljuri";
+
+        System.out.println(auto3.mostrarInfo()+" y el pais de origen es "+auto3.getPaisMarca());
+        
+        
+        var auto4 = new Auto();
+        auto4.placa="PGE547";
+        auto4.marca="Pagani";
+        auto4.nombrePropietario="Antonio Abad";
+
+        System.out.println(auto4.mostrarInfo()+" y el pais de origen es "+auto4.getPaisMarca());
+    
+        
+        
+        
+        
+        
+        
+        
+        
+    }}
+    
+    
+    
+
+    
