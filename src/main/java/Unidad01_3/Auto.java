897e4f947706184df63daf210a97d67b4ac59ac2
sebas <sebas@172.16.218.64>
2022-10-13 12:13:45 -0500

    metodos marcas+pagani

--- src/main/java/Unidad01_3/Auto.java
+++ src/main/java/Unidad01_3/Auto.java
@@ -13,50 +13,88 @@ public class Auto {
     String marca;
     String placa;
     String nombrePropietario;
+    int year;
+    String color;
+    double precio;
 
     public String mostrarInfo() {
-        var retorno = " El auto tiene placa: "+this.placa+" su marca es "
-                +this.marca+" y su propietario es: "+this.nombrePropietario;
-        
+        var retorno = " El auto tiene placa: " + this.placa + " su marca es "
+                + this.marca + " y su propietario es: " + this.nombrePropietario;
+
         return retorno;
     }
-        
+
     public String getProvincia() {
         var retorno = "";
+        var primeraLetra = "";
+        primeraLetra = this.placa.substring(0, 1);
+        switch (primeraLetra) {
+            case "A":
+                retorno = "Azuay";
+                break;
+            case "B":
+                retorno = "Bolivar";
+                break;
+            case "U":
+                retorno = "Cañar";
+                break;
+            case "C":
+                retorno = "Carchi";
+                break;
+            case "X":
+                retorno = "Cotopaxi";
+                break;
+            case "H":
+                retorno = "Chimborazo";
+                break;
+            case "O":
+                retorno = "EL Oro";
+                break;
+            default:
+                retorno = "Provincia no definida";
+        }
+
         return retorno;
-       
-    } 
+
+    }
+
     public String getPaisMarca() {
         var retorno = "";
-        if (this.marca.equals("Chevrolet")) 
+        if (this.marca.equals("Chevrolet")) {
             retorno = "EE.UU";
-            else 
-            if (this.marca.equals("Toyota")) 
-                retorno = "Japón";
-                else 
-                if (this.marca.equals("Jetour"))
-                    retorno="China";
-                    else
-                    if (this.marca.equals("Mercedez Benz"))
-                        retorno="Alemania";
-                        else
-                        if (this.marca.equals("Pagani"))
-                            retorno="Italia";
+        } else if (this.marca.equals("Toyota")) {
+            retorno = "Japón";
+        } else if (this.marca.equals("Jetour")) {
+            retorno = "China";
+        } else if (this.marca.equals("Mercedez Benz")) {
+            retorno = "Alemania";
+        } else if (this.marca.equals("Pagani")) {
+            retorno = "Italia";
+        } else {
+            retorno = "No definido";
+        }
 
-        else
-        retorno = "No definido";
-            
-        
-    return retorno;
+        return retorno;
     }
-        
-    
-public int getNumeroContinente() {
+    public int getNumeroContinente(){
         var retorno=0;
-        
         return retorno;
         
         
+        
+        
+        
+        
+        
     }
-
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
